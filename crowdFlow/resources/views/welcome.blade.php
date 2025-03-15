2587a952d710d8fb7c4c43cd3c43a8a05f0e39f1
Pasaran Razvan <pasaran@Pasarans-MacBook-Air.local>
2025-03-15 11:11:24 +0200

    laravel setup for the product

---
+++ crowdFlow/resources/views/welcome.blade.php
@@ -0,0 +1,72 @@
+<section class="py-12 bg-white sm:py-16 lg:py-20 xl:py-20">
+    <div class="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+        <div class="grid items-center grid-cols-1 gap-y-12 lg:grid-cols-2 lg:gap-x-16 xl:gap-x-24">
+            <div class="lg:order-2">
+                <div class="grid gap-4 px-8 pt-12 bg-gray-200 rounded-3xl place-items-end sm:px-16 sm:pt-20">
+                    <img class="w-full" src="https://landingfoliocom.imgix.net/store/collection/saasui/images/application/5/phone-mockup.svg" alt="" />
+                </div>
+            </div>
+
+            <div class="lg:order-1">
+                <h2 class="text-3xl font-semibold tracking-tight text-gray-900 sm:text-4xl lg:text-5xl">What are you waiting for, get the app now!</h2>
+                <p class="mt-4 text-base font-normal leading-7 text-gray-600 lg:text-lg lg:mt-6 lg:leading-8">Clarity gives you the blocks & components you need to create a truly professional website, landing page or panel for your SaaS and gives the blocks.</p>
+
+                <div class="inline-grid grid-cols-1 gap-6 mt-8 sm:grid-cols-2 sm:mt-12">
+                    <div class="flex items-center">
+                        <svg class="w-6 h-6 text-blue-600 shrink-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                            <path
+                                fill-rule="evenodd"
+                                d="M6.267 3.455a3.066 3.066 0 001.745-.723 3.066 3.066 0 013.976 0 3.066 3.066 0 001.745.723 3.066 3.066 0 012.812 2.812c.051.643.304 1.254.723 1.745a3.066 3.066 0 010 3.976 3.066 3.066 0 00-.723 1.745 3.066 3.066 0 01-2.812 2.812 3.066 3.066 0 00-1.745.723 3.066 3.066 0 01-3.976 0 3.066 3.066 0 00-1.745-.723 3.066 3.066 0 01-2.812-2.812 3.066 3.066 0 00-.723-1.745 3.066 3.066 0 010-3.976 3.066 3.066 0 00.723-1.745 3.066 3.066 0 012.812-2.812zm7.44 5.252a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                                clip-rule="evenodd"
+                            />
+                        </svg>
+                        <span class="text-lg font-medium text-gray-800 ml-2.5"> Easy transactions </span>
+                    </div>
+
+                    <div class="flex items-center">
+                        <svg class="w-6 h-6 text-blue-600 shrink-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                            <path
+                                fill-rule="evenodd"
+                                d="M6.267 3.455a3.066 3.066 0 001.745-.723 3.066 3.066 0 013.976 0 3.066 3.066 0 001.745.723 3.066 3.066 0 012.812 2.812c.051.643.304 1.254.723 1.745a3.066 3.066 0 010 3.976 3.066 3.066 0 00-.723 1.745 3.066 3.066 0 01-2.812 2.812 3.066 3.066 0 00-1.745.723 3.066 3.066 0 01-3.976 0 3.066 3.066 0 00-1.745-.723 3.066 3.066 0 01-2.812-2.812 3.066 3.066 0 00-.723-1.745 3.066 3.066 0 010-3.976 3.066 3.066 0 00.723-1.745 3.066 3.066 0 012.812-2.812zm7.44 5.252a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                                clip-rule="evenodd"
+                            />
+                        </svg>
+                        <span class="text-lg font-medium text-gray-800 ml-2.5"> Received money </span>
+                    </div>
+
+                    <div class="flex items-center">
+                        <svg class="w-6 h-6 text-blue-600 shrink-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                            <path
+                                fill-rule="evenodd"
+                                d="M6.267 3.455a3.066 3.066 0 001.745-.723 3.066 3.066 0 013.976 0 3.066 3.066 0 001.745.723 3.066 3.066 0 012.812 2.812c.051.643.304 1.254.723 1.745a3.066 3.066 0 010 3.976 3.066 3.066 0 00-.723 1.745 3.066 3.066 0 01-2.812 2.812 3.066 3.066 0 00-1.745.723 3.066 3.066 0 01-3.976 0 3.066 3.066 0 00-1.745-.723 3.066 3.066 0 01-2.812-2.812 3.066 3.066 0 00-.723-1.745 3.066 3.066 0 010-3.976 3.066 3.066 0 00.723-1.745 3.066 3.066 0 012.812-2.812zm7.44 5.252a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                                clip-rule="evenodd"
+                            />
+                        </svg>
+                        <span class="text-lg font-medium text-gray-800 ml-2.5"> Flexible for use </span>
+                    </div>
+
+                    <div class="flex items-center">
+                        <svg class="w-6 h-6 text-blue-600 shrink-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                            <path
+                                fill-rule="evenodd"
+                                d="M6.267 3.455a3.066 3.066 0 001.745-.723 3.066 3.066 0 013.976 0 3.066 3.066 0 001.745.723 3.066 3.066 0 012.812 2.812c.051.643.304 1.254.723 1.745a3.066 3.066 0 010 3.976 3.066 3.066 0 00-.723 1.745 3.066 3.066 0 01-2.812 2.812 3.066 3.066 0 00-1.745.723 3.066 3.066 0 01-3.976 0 3.066 3.066 0 00-1.745-.723 3.066 3.066 0 01-2.812-2.812 3.066 3.066 0 00-.723-1.745 3.066 3.066 0 010-3.976 3.066 3.066 0 00.723-1.745 3.066 3.066 0 012.812-2.812zm7.44 5.252a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                                clip-rule="evenodd"
+                            />
+                        </svg>
+                        <span class="text-lg font-medium text-gray-800 ml-2.5"> Record documents </span>
+                    </div>
+                </div>
+
+                <div class="flex flex-col mt-8 space-y-5 sm:mt-12 sm:space-y-0 sm:space-x-5 sm:flex-row">
+                    <a href="#" title="" class="transition-all duration-200 hover:-translate-y-1" target="_blank" rel="noopener">
+                        <img class="object-cover w-auto h-16" src="https://landingfoliocom.imgix.net/store/collection/saasui/images/application/3/app-store.svg" alt="" />
+                    </a>
+
+                    <a href="#" title="" class="transition-all duration-200 hover:-translate-y-1" target="_blank" rel="noopener">
+                        <img class="object-cover w-auto h-16" src="https://landingfoliocom.imgix.net/store/collection/saasui/images/application/3/play-store.svg" alt="" />
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
